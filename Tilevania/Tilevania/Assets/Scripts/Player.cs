f498f5e6508f59c723bd4beb1462bcb8a26c4648
avilla <alberto.villa1@st.com>
2020-05-22 12:42:37 +0200

    Add run/idle animation states in code

--- Tilevania/Tilevania/Assets/Scripts/Player.cs
+++ Tilevania/Tilevania/Assets/Scripts/Player.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Configurations
     [SerializeField] float runSpeed = 5f;
 
+    // State
+    bool isAlive = true;
+
+    // Cached components references
     Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
+
+    // Message then methods
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +36,9 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal"); // value is -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody2D.velocity.y);
         myRigidbody2D.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
     }
 
     private void FlipSprite()
