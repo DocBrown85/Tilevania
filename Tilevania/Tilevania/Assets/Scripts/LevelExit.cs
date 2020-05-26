c4e30fab93d6eff857f5da60bbe9a5f0a092fa91
avilla <alberto.villa1@st.com>
2020-05-26 11:27:26 +0200

    Add level exit portal

---
+++ Tilevania/Tilevania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float levelExitSlowMotionFactor = 0.2f;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = levelExitSlowMotionFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
